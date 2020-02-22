3cdb451ce87b864598ce313d315093ffc64ae1e6
srehan07 <60334400+srehan07@users.noreply.github.com>
2020-02-21 21:24:17 -0600

    week6
    week6

---
+++ src/ATMState.java
@@ -0,0 +1,7 @@
+
+public interface ATMState {
+    void insertCard();
+    void ejectCard();
+    void insertPin(int pinEntered);
+    void requestCash(int cashToWithdraw);
+}
---
+++ src/GetATMData.java
@@ -0,0 +1,5 @@
+public interface GetATMData
+{
+  public ATMState getATMState();
+  public int getCashInMachine();
+}
